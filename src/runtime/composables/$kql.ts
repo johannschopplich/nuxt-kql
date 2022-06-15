67af7d12a7c1685580bea18698fc3fabf1464e27
Johann Schopplich <mail@johannschopplich.com>
2022-06-15 23:35:52 +0200

    chore: split composables into files

--- src/runtime/composables/$kql.ts
+++ src/runtime/composables/$kql.ts
@@ -1,7 +1,5 @@
 import { hash as ohash } from 'ohash'
-import type { KqlPrivateFetchOptions, KqlPublicFetchOptions, KqlQueryRequest, KqlQueryResponse } from '../types'
-import { assertKqlPublicConfig, getAuthHeaders, normalizeHeaders } from '../utils'
-import type { ModuleOptions } from '../../module'
+import type { KqlPrivateFetchOptions, KqlQueryRequest, KqlQueryResponse } from '../types'
 import { useRuntimeConfig } from '#app'
 
 interface InternalState<T> {
@@ -39,29 +37,13 @@ export function $kql<T = KqlQueryResponse>(
     return state.promiseMap.get(key)
 
   const request = $fetch<T>(kql.apiRoute, { method: 'POST', body })
-    .then((r) => {
-      queries[key] = r
+    .then((response) => {
+      queries[key] = response
       state.promiseMap.delete(key)
-      return r
+      return response
     })
 
   state.promiseMap.set(key, request)
 
   return request
-}
-
-export function $publicKql<T = KqlQueryResponse>(
-  query: KqlQueryRequest,
-  opts: KqlPublicFetchOptions = {},
-): Promise<T> {
-  const { public: { kql } } = useRuntimeConfig()
-  assertKqlPublicConfig(kql as ModuleOptions)
-
-  return $fetch<T>(kql.kirbyEndpoint, {
-    ...opts,
-    baseURL: kql.kirbyUrl,
-    method: 'POST',
-    body: query,
-    headers: { ...normalizeHeaders(opts.headers), ...getAuthHeaders(kql as ModuleOptions) },
-  })
 }
---
+++ src/runtime/composables/$publicKql.ts
@@ -0,0 +1,20 @@
+import type { KqlPublicFetchOptions, KqlQueryRequest, KqlQueryResponse } from '../types'
+import { assertKqlPublicConfig, getAuthHeaders, headersToObject } from '../utils'
+import type { ModuleOptions } from '../../module'
+import { useRuntimeConfig } from '#app'
+
+export function $publicKql<T = KqlQueryResponse>(
+  query: KqlQueryRequest,
+  opts: KqlPublicFetchOptions = {},
+): Promise<T> {
+  const { public: { kql } } = useRuntimeConfig()
+  assertKqlPublicConfig(kql as ModuleOptions)
+
+  return $fetch<T>(kql.kirbyEndpoint, {
+    ...opts,
+    baseURL: kql.kirbyUrl,
+    method: 'POST',
+    body: query,
+    headers: { ...headersToObject(opts.headers), ...getAuthHeaders(kql as ModuleOptions) },
+  })
+}
