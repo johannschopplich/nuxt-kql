08a794623592dc9898046f7093bc1e7241c4120c
Johann Schopplich <mail@johannschopplich.com>
2022-09-26 08:31:38 +0200

    feat: merge `useKirbyData` with `usePublicKirbyData`

--- src/runtime/composables/useKirbyData.ts
+++ src/runtime/composables/useKirbyData.ts
@@ -5,8 +5,9 @@ import type { NitroFetchRequest } from 'nitropack'
 import type { AsyncData, UseFetchOptions } from 'nuxt/app'
 import { resolveUnref } from '@vueuse/core'
 import type { MaybeComputedRef } from '@vueuse/core'
-import { headersToObject, kirbyApiRoute } from '../utils'
-import { useFetch } from '#imports'
+import { buildAuthHeader, headersToObject, kirbyApiRoute } from '../utils'
+import type { ModuleOptions } from '../../module'
+import { useFetch, useRuntimeConfig } from '#imports'
 
 export type UseKirbyDataOptions<T> = Omit<
   UseFetchOptions<T>,
@@ -18,13 +19,42 @@ export type UseKirbyDataOptions<T> = Omit<
   | 'response'
   | 'transform'
   | keyof Omit<globalThis.RequestInit, 'headers'>
->
+> & {
+  /**
+   * Enable client-side requests to the API
+   */
+  client?: boolean
+}
 
 export function useKirbyData<T = any>(
   uri: MaybeComputedRef<string>,
   opts: UseKirbyDataOptions<T> = {},
 ) {
   const _uri = computed(() => resolveUnref(uri).replace(/^\//, ''))
+
+  if (!_uri.value)
+    console.error('[useKirbyData] Empty Kirby URI')
+
+  if (opts.client) {
+    const { kql } = useRuntimeConfig().public
+
+    if (!kql.clientRequests)
+      throw new Error('Fetching from Kirby client-side isn\'t allowed. Enable it by setting "clientRequests" to "true".')
+
+    return useFetch<T, FetchError, NitroFetchRequest, T>(_uri.value, {
+      ...opts,
+      key: hash(_uri.value),
+      baseURL: kql.url,
+      headers: {
+        ...headersToObject(opts.headers),
+        ...buildAuthHeader({
+          auth: kql.auth as ModuleOptions['auth'],
+          token: kql.token,
+          credentials: kql.credentials,
+        }),
+      },
+    }) as AsyncData<T, true | FetchError>
+  }
 
   return useFetch<T, FetchError, NitroFetchRequest, T>(kirbyApiRoute, {
     ...opts,
