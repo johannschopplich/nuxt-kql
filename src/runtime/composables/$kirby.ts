624b4d32d309b66fb07234e89ee5331bdba01569
Johann Schopplich <mail@johannschopplich.com>
2022-09-26 14:37:43 +0200

    feat: hooks like `onRequest` for `$kirby` composable

--- src/runtime/composables/$kirby.ts
+++ src/runtime/composables/$kirby.ts
@@ -1,9 +1,17 @@
 import { hash } from 'ohash'
+import type { FetchOptions } from 'ohmyfetch'
 import { buildAuthHeader, headersToObject, kirbyApiRoute } from '../utils'
 import type { ModuleOptions } from '../../module'
 import { useNuxtApp, useRuntimeConfig } from '#imports'
 
-export interface KirbyFetchOptions {
+export type KirbyFetchOptions = Pick<
+  FetchOptions,
+  | 'onRequest'
+  | 'onRequestError'
+  | 'onResponse'
+  | 'onResponseError'
+  | 'headers'
+> & {
   /**
    * Custom headers to send with the request
    */
@@ -19,26 +27,12 @@ export function $kirby<T = any>(
   uri: string,
   opts: KirbyFetchOptions = {},
 ): Promise<T> {
-  if (opts.client) {
-    const { kql } = useRuntimeConfig().public
-
-    if (!kql.clientRequests)
-      throw new Error('Fetching from Kirby client-side isn\'t allowed. Enable it by setting "clientRequests" to "true".')
-
-    return $fetch<T>(uri, {
-      baseURL: kql.url,
-      headers: {
-        ...headersToObject(opts.headers),
-        ...buildAuthHeader({
-          auth: kql.auth as ModuleOptions['auth'],
-          token: kql.token,
-          credentials: kql.credentials,
-        }),
-      },
-    })
-  }
-
   const nuxt = useNuxtApp()
+  const { kql } = useRuntimeConfig().public
+
+  if (opts.client && !kql.clientRequests)
+    throw new Error('Fetching from Kirby client-side isn\'t allowed. Enable it by setting "clientRequests" to "true".')
+
   nuxt._kqlPromises = nuxt._kqlPromises || {}
   const key = `$kirby${hash(uri)}`
 
@@ -48,13 +42,32 @@ export function $kirby<T = any>(
   if (key in nuxt._kqlPromises)
     return nuxt._kqlPromises[key]
 
-  const request = $fetch(kirbyApiRoute, {
+  const fetchOptions: FetchOptions = {
+    ...opts,
     method: 'POST',
     body: {
       uri,
       headers: headersToObject(opts.headers),
     },
-  }).then((response) => {
+  }
+
+  const publicFetchOptions: FetchOptions = {
+    ...opts,
+    baseURL: kql.url,
+    headers: {
+      ...headersToObject(opts.headers),
+      ...buildAuthHeader({
+        auth: kql.auth as ModuleOptions['auth'],
+        token: kql.token,
+        credentials: kql.credentials,
+      }),
+    },
+  }
+
+  const request = $fetch(
+    opts.client ? uri : kirbyApiRoute,
+    opts.client ? publicFetchOptions : fetchOptions,
+  ).then((response) => {
     nuxt.payload.data![key] = response
     delete nuxt._kqlPromises[key]
     return response
