5210d6c87cb3cdee320d0ba6fa8747eb3dc1b604
Johann Schopplich <mail@johannschopplich.com>
2022-11-16 16:47:54 +0100

    chore: upgrade to Nuxt 3 stable

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -1,8 +1,12 @@
+import { fileURLToPath } from 'node:url'
 import { describe, expect, it } from 'vitest'
-import { $fetch } from '@nuxt/test-utils'
+import { $fetch, setup } from '@nuxt/test-utils'
 
 describe('nuxt-kql', async () => {
-
+  await setup({
+    server: true,
+    rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
+  })
 
   it('fetches queries with $kql', async () => {
     const html = await $fetch('/test/$kql')
