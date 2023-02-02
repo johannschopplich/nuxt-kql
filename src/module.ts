4b0502b7aea5c1d85f94ce9a145e3b94a5cf3deb
Johann Schopplich <mail@johannschopplich.com>
2023-02-02 17:51:10 +0100

    chore: uprade dependencies

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { join } from 'path'
+import { join } from 'node:path'
 import { defu } from 'defu'
 import { pascalCase } from 'scule'
 import { addImportsDir, addServerHandler, addTemplate, createResolver, defineNuxtModule } from '@nuxt/kit'
