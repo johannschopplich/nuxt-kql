54f4fd85308958603f5fc32631c4d01db4806d92
Johann Schopplich <mail@johannschopplich.com>
2023-03-14 20:25:34 +0100

    docs: add Plausible analytics

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,6 +8,8 @@ import {
   releases,
 } from './meta'
 
+const url = new URL(ogUrl)
+
 export default defineConfig({
   lang: 'en-US',
   title: name,
@@ -23,6 +25,8 @@ export default defineConfig({
     ['meta', { name: 'twitter:description', content: description }],
     ['meta', { name: 'twitter:image', content: ogImage }],
     ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    // Plausible analytics
+    ['script', { 'src': 'https://plausible.io/js/script.js', 'defer': '', 'data-domain': url.hostname }],
   ],
 
   themeConfig: {
