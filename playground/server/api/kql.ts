b8be3a28eced20db1bc18f956a7376e1171b06f7
Johann Schopplich <mail@johannschopplich.com>
2024-03-12 21:35:32 +0100

    chore: ease linting

--- playground/server/api/kql.ts
+++ playground/server/api/kql.ts
@@ -1,4 +1,7 @@
+/* eslint-disable ts/prefer-ts-expect-error */
+// @ts-ignore: CI fails to resolve the import
 export default defineEventHandler(() => {
+  // @ts-ignore: CI fails to resolve the import
   return $kql({
     query: 'site',
   })
