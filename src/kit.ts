fed60c1d3fa2173663d25d3e0326b2cc62a79cf5
Johann Schopplich <mail@johannschopplich.com>
2024-03-20 07:51:51 +0100

    fix: `logger` inferred type

--- src/kit.ts
+++ src/kit.ts
@@ -1,3 +1,4 @@
 import { useLogger } from '@nuxt/kit'
+import type { ConsolaInstance } from 'consola'
 
-export const logger = useLogger('nuxt-kql')
+export const logger: ConsolaInstance = useLogger('nuxt-kql')
